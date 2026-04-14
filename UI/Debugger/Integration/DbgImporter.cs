d57bb9e0a3be8827637c61c95da8ab57d48c5fe1
NovaSquirrel <bushytail@novasquirrel.com>
2026-04-14 02:12:06 -0400

    Debugger: Try to load dbg files when running FDS games

--- UI/Debugger/Integration/DbgImporter.cs
+++ UI/Debugger/Integration/DbgImporter.cs
@@ -918,7 +918,7 @@ namespace Mesen.Debugger.Integration
 		{
 			DbgImporter? importer = romFormat switch {
 				RomFormat.Sfc => new SnesDbgImporter(romFormat),
-				RomFormat.iNes or RomFormat.Nsf or RomFormat.VsSystem or RomFormat.VsDualSystem => new NesDbgImporter(romFormat),
+				RomFormat.iNes or RomFormat.Nsf or RomFormat.VsSystem or RomFormat.VsDualSystem or RomFormat.Fds => new NesDbgImporter(romFormat),
 				RomFormat.Pce or RomFormat.PceHes => new PceDbgImporter(romFormat),
 				_ => null
 			};
